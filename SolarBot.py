39da2e6c2a19b52b43e61d650bb940592dc42247
sasha4043 <stal443@gmail.com>
2020-05-24 20:19:29 +0500

    Update SolarBot.py
    Сделал код чуть более читаемым

--- SolarBot.py
+++ SolarBot.py
@@ -28,28 +28,45 @@ for event in longpoll.listen():
 		id=event.obj.message['from_id']
 		if msg==":пинг":
 			if event.from_user:
-				vk.messages.send(user_id=id, random_id=random.randint(0, 2**64), message="Понг")
+				vk.messages.send(user_id=id, 
+						 random_id=random.randint(0, 2**64), 
+						 message="Понг")
 			elif event.from_chat:
-				vk.messages.send(chat_id=event.chat_id, random_id=random.randint(0, 2**64), message="Понг")
+				vk.messages.send(chat_id=event.chat_id, 
+						 random_id=random.randint(0, 2**64), 
+						 message="Понг")
 		elif msg==":кик":
 			if 'reply_message' in event.obj.message and event.from_chat:
 				if event.from_chat and id==381510409:
-					vk.messages.removeChatUser(chat_id=event.chat_id, member_id=event.obj.message['reply_message']['from_id'])
+					vk.messages.removeChatUser(chat_id=event.chat_id, 
+								   member_id=event.obj.message['reply_message']['from_id'])
 				else:
-					vk.messages.send(chat_id=event.chat_id, random_id=random.randint(0, 2**64), message="Пшёл нах")
+					vk.messages.send(chat_id=event.chat_id, 
+							 random_id=random.randint(0, 2**64), 
+							 message="Пшёл нах")
 			else:
-					vk.messages.send(chat_id=event.chat_id, random_id=random.randint(0, 2**64), message="А кого?")
+					vk.messages.send(chat_id=event.chat_id, 
+							 random_id=random.randint(0, 2**64), 
+							 message="А кого?")
 		elif msg==":id":
 			if 'reply_message' in event.obj.message:
 				if event.from_user:
-					vk.messages.send(user_id=id, random_id=random.randint(0, 2**64), message=str(event.obj.message['reply_message']['from_id']))
+					vk.messages.send(user_id=id, 
+							 random_id=random.randint(0, 2**64), 
+							 message=str(event.obj.message['reply_message']['from_id']))
 				elif event.from_chat:
-					vk.messages.send(chat_id=event.chat_id, random_id=random.randint(0, 2**64), message=str(event.obj.message['reply_message']['from_id']))
+					vk.messages.send(chat_id=event.chat_id, 
+							 random_id=random.randint(0, 2**64), 
+							 message=str(event.obj.message['reply_message']['from_id']))
 			else:
 				if event.from_user:
-					vk.messages.send(user_id=id, random_id=random.randint(0, 2**64), message=str(id))
+					vk.messages.send(user_id=id, 
+							 random_id=random.randint(0, 2**64), 
+							 message=str(id))
 				elif event.from_chat:
-					vk.messages.send(chat_id=event.chat_id, random_id=random.randint(0, 2**64), message=str(id))
+					vk.messages.send(chat_id=event.chat_id, 
+							 random_id=random.randint(0, 2**64), 
+							 message=str(id))
 		elif msg==":probe":
 			stm="Просто сообщение"
 			if 'reply_message' in event.obj.message:
@@ -57,6 +74,10 @@ for event in longpoll.listen():
 			elif 'fwd_messages' in event.obj.message:
 				stm="Пересланные сообщения от "+fios(event.obj.message['fwd_messages'][i]['from_id'] for i in range(len(event.obj.message['fwd_messages'])))
 			if event.from_user:
-				vk.messages.send(user_id=id, random_id=random.randint(0, 2**64), message=stm+" в ЛС")
+				vk.messages.send(user_id=id, 
+						 random_id=random.randint(0, 2**64), 
+						 message=stm+" в ЛС")
 			elif event.from_chat:
-				vk.messages.send(chat_id=event.chat_id, random_id=random.randint(0, 2**64), message=stm+" в беседу")
+				vk.messages.send(chat_id=event.chat_id, 
+						 random_id=random.randint(0, 2**64), 
+						 message=stm+" в беседу")
