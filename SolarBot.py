59bc9644ad94f8fa7cad437ec96bfeed8b3a9598
sasha4043 <stal443@gmail.com>
2020-05-24 18:51:28 +0500

    Update SolarBot.py

--- SolarBot.py
+++ SolarBot.py
@@ -11,6 +11,16 @@ vk = vk_session.get_api()
 def fio(uid):
 	return vk.users.get(user_ids=uid)[0]['first_name']+" "+vk.users.get(user_ids=uid)[0]['last_name']
 
+def fios(uid):
+	sps=""
+	j=0
+	for i in uid:
+		if j>0:
+			sps+=", "
+		sps+=vk.users.get(user_ids=i)[0]['first_name']+" "+vk.users.get(user_ids=i)[0]['last_name']
+		j+=1
+	return sps
+
 for event in longpoll.listen():
 	if event.type == VkBotEventType.MESSAGE_NEW:
 		print(event.obj.message)
@@ -23,7 +33,7 @@ for event in longpoll.listen():
 				vk.messages.send(chat_id=event.chat_id, random_id=random.randint(0, 2**64), message="Понг")
 		elif msg==":кик":
 			if 'reply_message' in event.obj.message and event.from_chat:
-				if event.from_chat and id==ид_админа:
+				if event.from_chat and id==381510409:
 					vk.messages.removeChatUser(chat_id=event.chat_id, member_id=event.obj.message['reply_message']['from_id'])
 				else:
 					vk.messages.send(chat_id=event.chat_id, random_id=random.randint(0, 2**64), message="Пшёл нах")
@@ -45,7 +55,7 @@ for event in longpoll.listen():
 			if 'reply_message' in event.obj.message:
 				stm="Ответ на сообщение от "+fio(event.obj.message['reply_message']['from_id'])
 			elif 'fwd_messages' in event.obj.message:
-				stm="Пересланные сообщения от "+fio(event.obj.message['reply_message']['from_id'])
+				stm="Пересланные сообщения от "+fios(event.obj.message['fwd_messages'][i]['from_id'] for i in range(len(event.obj.message['fwd_messages'])))
 			if event.from_user:
 				vk.messages.send(user_id=id, random_id=random.randint(0, 2**64), message=stm+" в ЛС")
 			elif event.from_chat:
